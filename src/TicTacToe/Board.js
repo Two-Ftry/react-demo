7c87f0bc12b960bd482fba7e1179d0053fc8f963
suifengfengye <1527261080@qq.com>
2017-06-27 01:46:58 +0800

    tuital continue

--- src/TicTacToe/Board.js
+++ src/TicTacToe/Board.js
@@ -6,7 +6,14 @@ import Squre from './Square';
 
 class Board extends Component {
 
-    renderRow (start, len) {
+    constructor () {
+        super();
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+    }
+
+    renderRow (start, len, curState) {
         const eles = [];
         for (let i = start; i < start + len; i++) {
             eles.push(i);
@@ -15,20 +22,33 @@ class Board extends Component {
             <div className="board-row clearfix" key={'row-' + start}>
                 {
                     eles.map((item) => {
-                        return <Squre index={item} key={item} gameState={this.props.gameState} />;
+                        return <Squre index={item}
+                                      value={curState[item]}
+                                      key={item}
+                                      gameState={this.props.gameState}
+                                      onClick={() => this.handleClick(item)}
+                        />;
                     })
                 }
             </div>
         );
     }
 
+    handleClick (i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.props.gameState.next;
+        this.setState({
+            squares
+        });
+    }
+
     render () {
-        const arr = [0, 3, 6,];
+        const arr = [0, 3, 6];
         return (
             <div className="board-box">
                 {
                     arr.map((item) => {
-                        return this.renderRow(item, 3);
+                        return this.renderRow(item, 3, this.state);
                     })
                 }
             </div>
--- src/TicTacToe/Square.js
+++ src/TicTacToe/Square.js
@@ -6,22 +6,15 @@ import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 
 class Square extends Component {
-    constructor () {
-        super();
-        this.state = {
-            value: ''
-        };
-    }
+
     render () {
         return (
             <div className="square-box" onClick={() => {
                 if (this.props.gameState.start) {
-                    this.setState({
-                        value: this.props.gameState.next
-                    });
+                    this.props.onClick();
                 }
             }}>
-                {this.state.value}
+                {this.props.value}
             </div>
         );
     }
