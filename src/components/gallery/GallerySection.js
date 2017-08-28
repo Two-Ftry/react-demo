1db4698846ce342edca302e3a8f33eddfbfaf6ae
suifengfengye <1527261080@qq.com>
2017-08-28 09:03:36 +0800

    done with rotate

--- src/components/gallery/GallerySection.js
+++ src/components/gallery/GallerySection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 import PageBar from '../page-bar/PageBar';
 
 require('./style.css');
@@ -10,95 +10,211 @@ imageDatas.forEach(item => {
   item.src = '../../images/' + item.fileName;
 });
 
-// 定义区块
-const blockArray = [];
-const imgWidth = 347;
-const imgHeight = 386;
-const initBlock = () => {
-  const root = document.documentElement || document.body;
-  const width = root.offsetWidth;
-  const height = root.offsetHeight;
-  // 左侧区域
-  blockArray.push({
-    start: [0 - imgWidth / 2, 0 - imgHeight / 2],
-    end: [width / 2 - 2 / 3 * imgWidth, height + imgHeight / 2]
-  });
-  // 右侧区域
-  blockArray.push({
-    start: [width / 2 + 2 / 3 * imgWidth, 0 - imgHeight / 2],
-    end: [width + imgWidth / 2, height + imgHeight / 2]
-  });
-  // 上侧区域
-  blockArray.push({
-    start: [width / 2 - imgWidth, 0 - imgHeight / 2],
-    end: [width / 2 + imgWidth, height / 2 - imgHeight * 2 / 3]
-  });
-  console.log('blockArray', blockArray);
+const getRandomData = (low, high) => {
+  if (typeof low === 'number' && typeof high === 'number') {
+    return Math.floor(Math.random() * (high - low) + low);
+  } else {
+    return 0;
+  }
 };
 
-const root = document.documentElement || document.body;
-const width = root.offsetWidth - 240;
-const height = root.offsetHeight - 360;
-// 随机定位
-const getRandomStyle = isSelected => {
-  const deg = Math.random() * 90 - 45;
-  const sty = {};
-  if (!isSelected) {
-    // 图片应该落到哪个区域
-    const areaIndex = Math.abs(Math.floor(Math.random() * 3));
-    const area = blockArray[areaIndex];
-    console.log('areaIndex:', areaIndex);
-    if (!area) {
-      return {};
-    }
-    const startX = area.start[0];
-    const startY = area.start[1];
-    const endX = area.end[0];
-    const endY = area.end[1];
-    sty.left = Math.floor(Math.random() * endX + startX) + 'px';
-    sty.top = Math.floor(Math.random() * endY + startY) + 'px';
-    console.log('left %s, top %s', sty.left, sty.top);
-    sty.opacity = '0.7';
-    sty.transform = `rotate(${deg}deg)`;
-  } else {
-    sty.top = '50%';
-    sty.left = '50%';
-    sty.transform = 'translate(-50%, -50%)';
-    sty.zIndex = 999;
-    sty.boxShadow = '0 0 12px #222';
-  }
-  return sty;
+// 获取旋转角度的随机值
+const getRandomRotate = (min, max) => {
+  min = min ? min : -30;
+  max = max ? max : 30;
+  return Math.floor(Math.random() * (max - min) + min);
 };
+
+/**
+ * 单个图片组件
+ */
+class ImgFigure extends React.Component {
+  render () {
+    const item = this.props.item;
+    const status = this.props.status;
+    const styleObj = status.pos;
+    if (status.rotate) {
+      styleObj.transform = `rotate(${status.rotate}deg)`;
+    }
+    return (
+      <li
+        style={styleObj}
+        className={this.props.index === this.props.selectedIndex ? 'selected-img' : ''}
+        key={item.fileName}
+        onClick={() => {
+          this.props.onClick(this.props.index);
+        }}
+      >
+        <img src={item.src}/>
+        <p>
+          {item.desc}
+        </p>
+      </li>
+    );
+  }
+}
+
 
 class GallerySection extends React.Component {
   constructor(props) {
     super(props);
+    this.Constant = {
+      area: { // 区域范围
+        center: {
+          pos: {
+            top: '50%',
+            left: '50%'
+          }
+        },
+        leftArea: {
+          start: [],
+          end: []
+        },
+        rightArea: {
+          start: [],
+          end: []
+        },
+        topArea: {
+          start: [],
+          end: []
+        }
+      }
+    };
+
     this.state = {
-      selectedIndex: Math.floor(Math.random() * 16)
+      selectedIndex: 0,
+      imageStateArray: [ // 每张图片的状态数组
+        // {
+        //   pos: {
+        //     top: 0,
+        //     left: 0
+        //   },
+        //   rotate: 20
+        // }
+      ]
     };
   }
 
+  componentDidMount () {
+    this.initAreaArrange();
+  }
+
+  // 初始化上区域、左区域、右区域的范围
+  initAreaArrange () {
+    const stage = ReactDOM.findDOMNode(this.refs.stage);
+    const width = stage.offsetWidth;
+    const height = stage.offsetHeight;
+    const imgFigure = ReactDOM.findDOMNode(this.refs.imgFigure0);
+    const imgWidth = imgFigure.offsetWidth;
+    const imgHeight = imgFigure.offsetHeight;
+    // const area = {};
+    // 左侧区域
+    this.Constant.area.leftArea = {
+        start: [0 - imgWidth / 2, 0 - imgHeight / 2],
+        end: [width / 2 - 3 / 2 * imgWidth, height - imgHeight / 2]
+    };
+    // 右侧区域
+    this.Constant.area.rightArea = {
+        start: [width / 2 + imgWidth, 0 - imgHeight / 2],
+        end: [width - imgWidth / 2, height - imgHeight / 2]
+    };
+    // 上侧区域
+    this.Constant.area.topArea = {
+        start: [width / 2 - imgWidth, 0 - imgHeight / 2],
+        end: [width / 2 + imgWidth, height / 2 - imgHeight * 2 / 3]
+      };
+      console.log('imgWidth, imgHeight', imgWidth, imgHeight);
+      console.log('Constant', this.Constant);
+
+    this.setImgsState(this.state.selectedIndex);
+  }
+
+  // 设置图片的状态信息
+  setImgsState (selectedIndex) {
+    const imageStateArray = [];
+    Object.assign(imageStateArray, this.state.imageStateArray);
+    // const imageStateArray = this.state.imageStateArray;
+    const stateArea = this.Constant.area;
+    const leftArea = stateArea.leftArea,
+          rightArea = stateArea.rightArea,
+          topArea = stateArea.topArea;
+    // 设置中间图片的位置信息
+    const selectedImg = imageStateArray.splice(selectedIndex, 1)[0];
+    selectedImg.pos = stateArea.center.pos;
+    selectedImg.rotate = 0;
+
+    // 设置顶部图片的位置信息(选取0-1张)
+    const topImagesCount = Math.floor(Math.random() * 2);
+    let topImg = null;
+    const topImgIndex = Math.floor(Math.random() * imageStateArray.length);
+    if (topImagesCount) {
+      topImg = imageStateArray.splice(topImgIndex, 1)[0];
+      if (!topImg.pos) {
+        topImg.pos = {};
+      }
+      topImg = {
+        pos: {
+          left: getRandomData(topArea.start[0], topArea.end[0]) + 'px',
+          top: getRandomData(topArea.start[1], topArea.end[1]) + 'px'
+        },
+        rotate: getRandomRotate()
+      };
+      // totest
+      // topImg.pos.left = getRandomData(topArea.start[0], topArea.end[0]) + 'px';
+      // topImg.pos.top = getRandomData(topArea.start[1], topArea.end[1]) + 'px';
+    }
+
+    // 设置左侧区域、右侧区域图片的位置信息
+    for (let i = 0, len = imageStateArray.length, k = len / 2; i < len; i++) {
+        let areaTemp = null;
+        if (i < k) { // 放在左边
+          areaTemp = leftArea;
+        } else { // 放在右边
+          areaTemp = rightArea;
+        }
+        imageStateArray[i] = {
+          pos: {
+            top: getRandomData(areaTemp.start[1], areaTemp.end[1]) + 'px',
+            left: getRandomData(areaTemp.start[0], areaTemp.end[0]) + 'px'
+          },
+          rotate: getRandomRotate()
+        }
+    }
+    if (topImg) {
+      imageStateArray.splice(topImgIndex, 0, topImg);
+    }
+    imageStateArray.splice(selectedIndex, 0, selectedImg);
+
+    // 设置状态
+    this.setState({
+      imageStateArray: imageStateArray
+    });
+  }
+
   render() {
-    initBlock();
+
     const imageList = imageDatas.map((item, index) => {
-      const pos = getRandomStyle(this.state.selectedIndex === index);
+      if (!this.state.imageStateArray[index]) {
+        this.state.imageStateArray[index] = {
+          pos: {
+            left: 0,
+            top: 0
+          },
+          rotate: 0
+        }
+      }
       return (
-        <li
-          style={pos}
-          key={item.fileName}
-          onClick={() => {
-            this.selectNewImage(index);
-          }}
-        >
-          <img src={item.src}/>
-          <p>
-            {item.desc}
-          </p>
-        </li>
+        <ImgFigure ref={'imgFigure' + index}
+                   key={'img-figure' + index}
+                   status={this.state.imageStateArray[index]}
+                   index={index}
+                   item={item}
+                   selectedIndex={this.state.selectedIndex}
+                   onClick={this.selectNewImage} />
       );
     });
     return (
-      <section className="gallery-section">
+      <section className="gallery-section" ref="stage">
         <ul className="image-list">
           {imageList}
         </ul>
@@ -113,11 +229,12 @@ class GallerySection extends React.Component {
 
   selectNewImage = index => {
     if (index === this.state.selectedIndex) {
-      // todo
+      // todo 翻转图片
     } else {
       this.setState({
         selectedIndex: index
       });
+      this.setImgsState(index);
     }
   };
 }
